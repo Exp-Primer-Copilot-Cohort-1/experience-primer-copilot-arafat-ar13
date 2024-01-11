603e0bb8572ef585ce474f0ca06be07514073449
Arafat Khan <anony.masked@hacker.8shield.net>
2024-01-11 23:09:43 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,6 @@
+// creating a function that will add two numbers and also subtract two numbers and return both as a list
+function calculateNumbers(num1, num2) {
+    let sum = num1 + num2;
+    let difference = num1 - num2;
+    return [sum, difference];
+} 
